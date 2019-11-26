5aec5f2b4e5b743b41a9c1ddfba7bbc12d8b8711
Irish Rufo <54094408+RufoIrish@users.noreply.github.com>
2019-11-26 13:54:53 +0800

    dfsdf

--- irish_vue/apiserver/createTables.js
+++ irish_vue/apiserver/createTables.js
@@ -2,15 +2,15 @@ var mysql = require('mysql');
 
 var con = mysql.createConnection({
   host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
+  user: "root",
+  password: "",
   database: "irishbacklogs"
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE backlogs (task VARCHAR(255), schedule VARCHAR(255), done BOOLEAN, nickname VARCHAR(255),id int NOT NULL AUTO_INCREMENT)";
+  var sql = "CREATE TABLE backlogs (id int NOT NULL AUTO_INCREMENT,task VARCHAR(255), schedule VARCHAR(255), done BOOLEAN, nickname VARCHAR(255),PRIMARY KEY (id))";
   con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
